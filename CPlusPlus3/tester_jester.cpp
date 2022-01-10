f4b5f4baebd7953f9b556d83eff261bfb5872e2e
AngieXuAU <angelinaxuwenlan@gmail.com>
2022-01-10 20:36:01 +1100

    Chapter 4 outputs testing

--- CPlusPlus3/tester_jester.cpp
+++ CPlusPlus3/tester_jester.cpp
@@ -1,11 +1,10 @@
 //
-// Created by angel on 28/12/2021, for testing ideas
+// Created by angel on 28/12/2021, for exercises testing
 //
 #define NEW_DEFINITION 0.089
 #include <cstdio>
-int Variable;
+int i = 6, j;
 int main(){
-    scanf("|%d|", &Variable);
-    printf("%5d", Variable);
-    return 0;
+    j = i += i;
+    printf("%d %d", i, j);
 }
